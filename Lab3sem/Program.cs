04d95783cbe83f4ca5114b33af457f7d022d4d31
TrampSirius <KarzanovArkadiy@mail.com>
2021-12-22 12:59:39 +0300

    Come closer.

---
+++ Lab3sem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Lab3sem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Delete("file2.txt"); // Удалить файл "file2.txt" 
+            StreamWriter sw = System.IO.File.CreateText("file.txt");
+            sw.Write("Пусть каждый день твой будет светлым, ");
+            sw.WriteLine("приятным, радостным и щедрым!");
+            sw.Close();
+            File.Move("file.txt", "file2.txt"); // Переименование
+            FileInfo fileInfo = new FileInfo("file2.txt");
+            Console.WriteLine("File {0} is {1} bytes in length, created on {2}",
+            fileInfo.FullName, fileInfo.Length, fileInfo.CreationTime);
+            Console.WriteLine("");
+            StreamReader sr = fileInfo.OpenText();
+            String s = sr.ReadLine();
+            while (s != null)
+            {
+                Console.WriteLine(s); s = sr.ReadLine();
+            }
+            sr.Close();
+        }
+    }
+}
